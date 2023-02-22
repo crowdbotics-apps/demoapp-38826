478b11145a980c63ba3613196895f167a0ccc428
crowdbotics-apps <team@crowdbotics.com>
2023-02-22 22:18:38 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+class Event(models.Model):
+    'Generated Model'
+    name = models.BigIntegerField()
+    date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True,)
+    updated_at = models.DateTimeField(auto_now=True,)
