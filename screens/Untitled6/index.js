5adc420dd7725174eda454b6e7fbe4818113b94e
Muhammad Shoaib <muhammad.shoaib@crowdbotics.com>
2023-04-12 11:55:08 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled6/index.js
+++ screens/Untitled6/index.js
@@ -27,8 +27,9 @@ const Untitled6 = () => {
           <Text style={styles.GygeDsOS}>Front-End Development</Text>
           <Text style={styles.OjKVCzaX}>April 12, 2023</Text>
         </View>
-        <View style={styles.pYBJdGTl}></View>
-      </ScrollView>
+        <View style={styles.pYBJdGTl}><View style={styles.CcvaYfiI}></View><Text style={styles.OwnbsEbC}>Project 2</Text><Text style={styles.AwQnYCtN}>Front-End
+Development</Text><Text style={styles.OSjukWhu}>April  10, 2023</Text></View>
+      <Text style={styles.qmGmlJxH}>Progress</Text><View style={styles.VcibujGW}><View style={styles.RpUMXqsD}></View><Text style={styles.ilzUzfrE}>Design Changes</Text><Text style={styles.BGOLThty}>2 Days ago</Text></View><View style={styles.HQstRfLH}><View style={styles.GgWRTYre}></View><Text style={styles.cQMCxqLA}>Design Changes</Text><Text style={styles.FRwIyXVN}>2 Days ago</Text></View></ScrollView>
     </SafeAreaView>;
 };
 
@@ -179,13 +180,13 @@ const styles = StyleSheet.create({
     color: "#777777",
     position: "absolute",
     top: 148,
-    left: 199
+    left: 198
   },
   MWfuaqfk: {
     height: 28,
     width: 39,
     backgroundColor: "#E4E4E4",
-    borderRadius: 2,
+    borderRadius: 3,
     color: "#777777",
     position: "absolute",
     top: 19,
@@ -205,13 +206,15 @@ const styles = StyleSheet.create({
   GygeDsOS: {
     width: 128,
     height: 37,
-    lineHeight: 14,
-    fontSize: 21,
+    lineHeight: 17,
+    fontSize: 16,
     borderRadius: 0,
     color: "#fffffd",
     position: "absolute",
     top: 72,
-    left: 15
+    left: 15,
+    letterSpacing: 3,
+    fontWeight: "700"
   },
   OjKVCzaX: {
     width: 100,
@@ -224,6 +227,158 @@ const styles = StyleSheet.create({
     top: 139,
     left: 17,
     letterSpacing: 1
+  },
+  CcvaYfiI: {
+    height: 30,
+    width: 43,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 3,
+    color: "#777777",
+    position: "absolute",
+    top: 19,
+    left: 16
+  },
+  OwnbsEbC: {
+    width: 62,
+    height: 18,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0,
+    color: "#fdfdfd",
+    position: "absolute",
+    top: 27,
+    left: 69
+  },
+  AwQnYCtN: {
+    width: 124,
+    height: 41,
+    lineHeight: 19,
+    fontSize: 16,
+    borderRadius: 0,
+    color: "#fdfdfd",
+    opacity: 1,
+    position: "absolute",
+    top: 68,
+    left: 13,
+    textAlign: "left",
+    fontWeight: "700",
+    letterSpacing: 2
+  },
+  fzfxPgUa: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  OSjukWhu: {
+    width: 121,
+    height: 19,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0,
+    letterSpacing: 1,
+    color: "#fbfbfb",
+    position: "absolute",
+    top: 137,
+    left: 11
+  },
+  qmGmlJxH: {
+    width: 91,
+    height: 24,
+    lineHeight: 19,
+    fontSize: 19,
+    borderRadius: 0,
+    fontWeight: "700",
+    letterSpacing: 1,
+    position: "absolute",
+    top: 336,
+    left: 12,
+    color: "#5b5454"
+  },
+  VcibujGW: {
+    height: 60,
+    width: 287,
+    backgroundColor: "#fffffa",
+    borderRadius: 13,
+    color: "#777777",
+    position: "absolute",
+    top: 365,
+    left: 38
+  },
+  HQstRfLH: {
+    height: 60,
+    width: 288,
+    backgroundColor: "#fefefb",
+    borderRadius: 13,
+    color: "#777777",
+    position: "absolute",
+    top: 435,
+    left: 38
+  },
+  RpUMXqsD: {
+    height: 33,
+    width: 34,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 5,
+    color: "#777777",
+    position: "absolute",
+    top: 14,
+    left: 13
+  },
+  ilzUzfrE: {
+    width: 107,
+    height: 16,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0,
+    position: "absolute",
+    top: 16,
+    left: 55
+  },
+  BGOLThty: {
+    width: 65,
+    height: 10,
+    lineHeight: 12,
+    fontSize: 12,
+    borderRadius: 0,
+    fontWeight: "200",
+    position: "absolute",
+    top: 32,
+    left: 55,
+    color: "#696464"
+  },
+  GgWRTYre: {
+    height: 35,
+    width: 34,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 5,
+    color: "#777777",
+    position: "absolute",
+    top: 13,
+    left: 14
+  },
+  cQMCxqLA: {
+    width: 106,
+    height: 13,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0,
+    position: "absolute",
+    top: 17,
+    left: 54
+  },
+  FRwIyXVN: {
+    width: 66,
+    height: 14,
+    lineHeight: 14,
+    fontSize: 12,
+    borderRadius: 0,
+    fontWeight: "200",
+    color: "#7b7575",
+    position: "absolute",
+    top: 33,
+    left: 53
   }
 });
 export default Untitled6;
