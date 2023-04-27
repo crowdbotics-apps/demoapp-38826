3e62b7d021f03f281ac526f8e81c00419ffc9ca1
Crowdbotics <engineering@crowdbotics.com>
2023-04-27 10:41:32 +0000

    Update store.

--- store/demoappAPI/api.js
+++ store/demoappAPI/api.js
@@ -1,73 +1,71 @@
-import axios from "axios";
+import axios from "axios"
 const demoappAPI = axios.create({
-  baseURL: "",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  baseURL: "https://demoapp-38826-prod.herokuapp.com",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+function api_v1_event_list(payload) {
+  return demoappAPI.get(`/api/v1/event/`)
+}
+function api_v1_event_create(payload) {
+  return demoappAPI.post(`/api/v1/event/`, payload.data)
+}
 function api_v1_login_create(payload) {
-  return demoappAPI.post(`/api/v1/login/`, payload.data);
+  return demoappAPI.post(`/api/v1/login/`, payload.data)
 }
-
 function api_v1_signup_create(payload) {
-  return demoappAPI.post(`/api/v1/signup/`, payload.data);
+  return demoappAPI.post(`/api/v1/signup/`, payload.data)
 }
-
 function rest_auth_user_retrieve(payload) {
-  return demoappAPI.get(`/rest-auth/user/`);
+  return demoappAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return demoappAPI.put(`/rest-auth/user/`, payload.data);
+  return demoappAPI.put(`/rest-auth/user/`, payload.data)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return demoappAPI.patch(`/rest-auth/user/`, payload.data);
+  return demoappAPI.patch(`/rest-auth/user/`, payload.data)
 }
-
 function api_docs_schema_retrieve(payload) {
-  return demoappAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+  return demoappAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
 }
-
 function rest_auth_login_create(payload) {
-  return demoappAPI.post(`/rest-auth/login/`, payload.data);
+  return demoappAPI.post(`/rest-auth/login/`, payload.data)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return demoappAPI.get(`/rest-auth/logout/`);
+  return demoappAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return demoappAPI.post(`/rest-auth/logout/`);
+  return demoappAPI.post(`/rest-auth/logout/`)
 }
-
+function api_v1_event_retrieve(payload) {
+  return demoappAPI.get(`/api/v1/event/${payload.id}/`)
+}
+function api_v1_event_update(payload) {
+  return demoappAPI.put(`/api/v1/event/${payload.id}/`, payload.data)
+}
+function api_v1_event_partial_update(payload) {
+  return demoappAPI.patch(`/api/v1/event/${payload.id}/`, payload.data)
+}
+function api_v1_event_destroy(payload) {
+  return demoappAPI.delete(`/api/v1/event/${payload.id}/`)
+}
 function rest_auth_registration_create(payload) {
-  return demoappAPI.post(`/rest-auth/registration/`, payload.data);
+  return demoappAPI.post(`/rest-auth/registration/`, payload.data)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return demoappAPI.post(`/rest-auth/password/reset/`, payload.data);
+  return demoappAPI.post(`/rest-auth/password/reset/`, payload.data)
 }
-
 function rest_auth_password_change_create(payload) {
-  return demoappAPI.post(`/rest-auth/password/change/`, payload.data);
+  return demoappAPI.post(`/rest-auth/password/change/`, payload.data)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return demoappAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
+  return demoappAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return demoappAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
+  return demoappAPI.post(`/rest-auth/registration/verify-email/`, payload.data)
 }
-
 export const apiService = {
+  api_v1_event_list,
+  api_v1_event_create,
   api_v1_login_create,
   api_v1_signup_create,
   rest_auth_user_retrieve,
@@ -77,9 +75,13 @@ export const apiService = {
   rest_auth_login_create,
   rest_auth_logout_retrieve,
   rest_auth_logout_create,
+  api_v1_event_retrieve,
+  api_v1_event_update,
+  api_v1_event_partial_update,
+  api_v1_event_destroy,
   rest_auth_registration_create,
   rest_auth_password_reset_create,
   rest_auth_password_change_create,
   rest_auth_password_reset_confirm_create,
   rest_auth_registration_verify_email_create
-};
+}
